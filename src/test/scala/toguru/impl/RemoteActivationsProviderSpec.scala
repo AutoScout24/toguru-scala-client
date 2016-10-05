b77bef2a7b68b130f3f3f8e98f852374de8753cb
Andreas Schroeder <aschroeder@autoscout24.com>
2016-10-05 16:10:53 +0200

    Introducing ToguruClient and circuit breaker
    - Adding a toguru client class that contains toggle info provider and activations provider.
    - Using a circuit breaker when connecting to the toguru server
    - Updating the Scaladocs and readme
    - Increasing code coverage

--- src/test/scala/toguru/impl/RemoteActivationsProviderSpec.scala
+++ src/test/scala/toguru/impl/RemoteActivationsProviderSpec.scala
@@ -1,11 +1,12 @@
 package toguru.impl
 
-import java.util.UUID
 import java.util.concurrent.Executors
 
+import org.http4s.server.blaze.BlazeBuilder
 import org.scalatest.{OptionValues, ShouldMatchers, WordSpec}
-import toguru.api.{ClientInfo, Condition, DefaultActivations, Toggle}
+import toguru.api.{Condition, DefaultActivations, Toggle}
 import toguru.impl.RemoteActivationsProvider.TogglePoller
+import scala.concurrent.duration._
 
 class RemoteActivationsProviderSpec extends WordSpec with OptionValues with ShouldMatchers {
 
@@ -82,10 +83,56 @@ class RemoteActivationsProviderSpec extends WordSpec with OptionValues with Shou
 
   "Creating activation provider" should {
     "succeed with a valid url" in {
-      val url = "localhost:9000/togglestate"
-
-      val provider = RemoteActivationsProvider(url)
+      val provider = RemoteActivationsProvider("http://localhost:9000")
       provider.close()
     }
+  }
+
+  "Created activation provider" should {
+    "poll remote url" in {
+      import org.http4s._
+      import org.http4s.dsl._
+      val service = HttpService {
+        case _ => Ok("""[{"id":"toggle-one","tags":{"team":"Toguru Team","services":"toguru"},"rolloutPercentage":20}]""")
+      }
+      val server = BlazeBuilder.bindHttp(9000, "localhost").mountService(service, "/togglestate").run
+
+      val rolloutCondition = Condition.UuidRange(1 to 20)
+
+      val provider = RemoteActivationsProvider("http://localhost:9000", pollInterval = 100.milliseconds)
+
+      waitFor(20, 100.millis) {
+        provider.apply() != DefaultActivations
+      }
+
+      provider.close()
+      server.shutdownNow()
+
+      val activations = provider.apply()
+
+      activations.apply(toggleOne) shouldBe rolloutCondition
+      activations.togglesFor("toguru") shouldBe Map(toggleOne.id -> rolloutCondition)
+    }
+  }
+
+
+  /**
+    *
+    * @param times how many times we want to try.
+    * @param wait how long to wait before the next try
+    * @param test returns true if test (finally) succeeded, false if we need to retry
+    */
+  def waitFor(times: Int, wait: FiniteDuration = 2.second)(test: => Boolean): Unit = {
+    val success = (1 to times).exists { i =>
+      if(test) {
+        true
+      } else {
+        if(i < times)
+          Thread.sleep(wait.toMillis)
+        false
+      }
+    }
+
+    success shouldBe true
   }
 }
